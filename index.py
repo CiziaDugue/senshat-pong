e77b890ea33564835c9e25e3ba89e926fc0e83e3
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2018-12-12 10:04:26 +0100

    boucle mvt balle

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
+import time
 from sense_hat import SenseHat
 
 sense = SenseHat()
 sense.clear()
+msleep = lambda x: time.sleep(x / 1000.0)
 
 #Initialisation des variables : coordonnées de la balle
 x = 4
@@ -9,3 +11,18 @@ y = 5
 
 #Affichage de la balle
 sense.set_pixel(x,y,255,0,0)
+
+#Boucle déplacement balle
+i = 1
+j = 1
+while x >= 0 and y >= 0 and x <= 7 and y <= 7:
+  x += i
+  y += j
+  msleep(400)
+  sense.clear()
+  sense.set_pixel(x,y,255,0,0)
+  if x == 0 or x == 7:
+    i = -i
+  if y == 0 or y == 7:
+    j = -j
+  
