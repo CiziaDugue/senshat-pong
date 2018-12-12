cf43b2415e9cd66c573cdf8a66aa002316068c1e
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2018-12-12 10:36:24 +0100

    ajout raquette

--- index.py
+++ index.py
@@ -8,23 +8,38 @@ sense.clear()
 msleep = lambda x: time.sleep(x / 1000.0)
 
 #Initialisation des variables : coordonnées de la balle
-x = 4
-y = 5
+x = 2
+y = 6
 
 #Affichage de la balle
 sense.set_pixel(x,y,255,0,0)
 
-#Boucle déplacement balle
+#Affichage de la raquette
+z = (0,1,2)
+sense.set_pixel(0,z[0],0,0,255)
+sense.set_pixel(0,z[1],0,0,255)
+sense.set_pixel(0,z[2],0,0,255)
+
+#Boucle déplacement balle & raquette
 i = 1
 j = 1
-while x >= 0 and y >= 0 and x <= 7 and y <= 7:
+while True:
   x += i
   y += j
-  msleep(400)
+  msleep(600)
   sense.clear()
   sense.set_pixel(x,y,255,0,0)
-  if x == 0 or x == 7:
+  sense.set_pixel(0,z[0],0,0,255)
+  sense.set_pixel(0,z[1],0,0,255)
+  sense.set_pixel(0,z[2],0,0,255)
+  if x == 7:
     i = -i
   if y == 0 or y == 7:
     j = -j
- 
+  if x == 0:
+    msleep(600)
+    sense.set_pixel(x,y,255,0,0)
+    sense.clear()
+    break
+  
+
